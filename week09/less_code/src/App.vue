58744acbb3c20a4989a42ec0d5a2b4d32545b3a2
yujie02 <yujie3@sugon.com>
2021-09-08 21:00:58 +0800

    提交第9周作业

---
+++ week09/less_code/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      websocket: null
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+    
+  },
+};
+</script>
+
---
+++ week09/less_code/src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+  input: <el-input placeholder="请输入内容" />,
+  select: <el-select placeholder="请选择"></el-select>
+}
